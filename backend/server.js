ebbad4d02b6491510defb86353b73dae25aa0e7b
RezaeiArshad <mehdirezaeiarshad@gmail.com>
2025-11-14 23:39:58 +0330

    trying some thing else

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use('/api/orders', orderRouter);
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-app.get('/*path', (req, res) =>
+app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
 );
 
